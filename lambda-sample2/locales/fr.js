193290efa1e7111809aba6c07f4c10c2e8da7f8c
abel <abel@kakaopage.com>
2022-02-17 15:19:01 +0900

    [create] unit test

---
+++ lambda-sample2/locales/fr.js
@@ -0,0 +1,4 @@
+{
+    "contenthome_universe_matching_type_today_mostviewed": "Titres les plus lus aujourd’hui",
+    "contenthome_universe_matching_type_today_mostpopular": "Titres les plus ajoutés aux favoris aujourd’hui"
+}
---
+++ lambda-sample2/locales/ko.js
@@ -0,0 +1,4 @@
+{
+    "contenthome_universe_matching_type_today_mostviewed": "오늘 사람들이 가장 많이 본 작품들",
+    "contenthome_universe_matching_type_today_mostpopular": "오늘 사람들이 가장 많이 찜한 작품들"
+}
